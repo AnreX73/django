4ef95ce80b1da62742ecb7c4034b1394d91bf3fa
AnreX73 <han220173@gmail.com>
2022-06-24 17:07:12 +0700

    modal window

---
+++ smstudio/smart/static/smart/js/main.js
@@ -0,0 +1,27 @@
+
+// let openModal = function open(modal){
+//     modal.style.display = 'block',
+//     console.log('work')
+// }
+// let closeModal = function close(modal){
+//     modal.style.display = 'none';
+// }
+// window.addEventListener("click", function(event) {
+//     if (event.target == mymodal_window2) {
+//         mymodal_window2.style.display = "none";
+//     }
+// });window.addEventListener("click", function(event) {
+//     if (event.target == mymodal_window) {
+//         mymodal_window.style.display = "none";
+//     }
+// });
+
+// let openButton1 = document.querySelector('#detailBtn1');
+// let closeButton1 = document.querySelector('.close_modal');
+// openButton1.addEventListener("click",()=>openModal(mymodal_window));
+// closeButton1.addEventListener("click",()=>closeModal(mymodal_window));
+// let openButton2 = document.querySelector('#detailBtn2');
+// let closeButton2 = document.querySelector('.close_modal2');
+// openButton2.addEventListener("click",()=>openModal(mymodal_window2));
+// closeButton2.addEventListener("click",()=>closeModal(mymodal_window2));
+
