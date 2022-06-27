55de9613b606d05f18558d218543e683472bcdb3
AnreX73 <han220173@gmail.com>
2022-06-27 08:18:42 +0700

    modal done!

--- smstudio/smart/static/smart/js/main.js
+++ smstudio/smart/static/smart/js/main.js
@@ -3,9 +3,9 @@
 !function(e){"function"!=typeof e.matches&&(e.matches=e.msMatchesSelector||e.mozMatchesSelector||e.webkitMatchesSelector||function(e){for(var t=this,o=(t.document||t.ownerDocument).querySelectorAll(e),n=0;o[n]&&o[n]!==t;)++n;return Boolean(o[n])}),"function"!=typeof e.closest&&(e.closest=function(e){for(var t=this;t&&1===t.nodeType;){if(t.matches(e))return t;t=t.parentNode}return null})}(window.Element.prototype);
 /* Получаем массив кнопок */
 document.addEventListener('DOMContentLoaded', function() {
-    var modalButtons = document.querySelectorAll('.price-link'),
+    var modalButtons = document.querySelectorAll('.js-open-modal'),
         closeButtons = document.querySelectorAll('.js-modal-close'),
-        overlay      = document.querySelector('.js-overlay-modal');
+        overlay      = document.querySelectorAll('.js-overlay-modal')
     /* Перебираем массив кнопок */
     modalButtons.forEach(function(item){
     /* Назначаем каждой кнопке обработчик клика */
@@ -13,17 +13,14 @@ document.addEventListener('DOMContentLoaded', function() {
         e.preventDefault();
         /* ищем модальное окно по атрибуту кнопки */
         var modalId = this.getAttribute('data-modal');
-        modalElem = document.querySelector('.extracontent-wrapper[data-modal="' + modalId + '"]');
+        modalElem = document.querySelector('.js-overlay-modal[data-modal="' + modalId + '"]');
         modalElem.style.display = 'block';
      });
 });
 closeButtons.forEach(function(item){
-
     item.addEventListener('click', function(e) {
        var parentModal = this.closest('.modal');
-       parentModal.style.display = 'none';
-
-       
+       parentModal.style.display = 'none';  
     });
 
  });
@@ -37,31 +34,16 @@ closeButtons.forEach(function(item){
     };
 }, false);
 
+overlay.forEach(function(item){
+    /* Назначаем каждой кнопке обработчик клика */
+    item.addEventListener('click', function(e) {
+        e.preventDefault();
+        /* ищем модальное окно по атрибуту кнопки */
+        var closeModal = this.closest('.modal');
+        closeModal.style.display = 'none';
+     });
 });
-// let openModal = function open(modal){
-//     modal.style.display = 'block',
-//     console.log('work')
-// }
-// let closeModal = function close(modal){
-//     modal.style.display = 'none';
-// }
-// window.addEventListener("click", function(event) {
-//     if (event.target == mymodal_window2) {
-//         mymodal_window2.style.display = "none";
-//     }
-// });window.addEventListener("click", function(event) {
-//     if (event.target == mymodal_window) {
-//         mymodal_window.style.display = "none";
-//     }
-// });
 
-// let openLink = document.querySelectorAll('.price-link');
-// console.log(openLink)
-// let closeButton1 = document.querySelector('.close_modal');
-// openButton1.addEventListener("click",()=>openModal(mymodal_window));
-// closeButton1.addEventListener("click",()=>closeModal(mymodal_window));
-// let openButton2 = document.querySelector('#detailBtn2');
-// let closeButton2 = document.querySelector('.close_modal2');
-// openButton2.addEventListener("click",()=>openModal(mymodal_window2));
-// closeButton2.addEventListener("click",()=>closeModal(mymodal_window2));
+});
+
 
